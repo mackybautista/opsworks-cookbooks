6e4de20c7c47c594c6e10fb8333717076c5b2024
mackybautista <macky@virtustructure.com>
2014-06-04 00:13:24 +0800

    updated config_beta

--- deploy/recipes/config_beta.rb
+++ deploy/recipes/config_beta.rb
@@ -2,6 +2,8 @@ bash "copy_config_beta" do
   user "root"
   cwd "/srv/www/rms_beta"
   code <<-EOH
-  sh deploy-beta.sh
+  yes | cp -Rf current/application/config/specifics/beta/database.php current/application/config/
+  yes | cp -Rf current/application/config/specifics/beta/config.php current/application/config/
+  chmod -Rf 777 current/
   EOH
 end
